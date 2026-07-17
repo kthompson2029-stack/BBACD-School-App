d8e10cc9c03bebb066356dbebe156d805cf78b75
kthompson2029-stack <kthompson2029@banneker.nyc>
2026-07-17 09:50:21 -0400

    Implement cache clearing on service worker activation
    Add activate event listener to clear old caches.

--- service-worker.js
+++ service-worker.js
@@ -25,3 +25,12 @@ self.addEventListener("fetch", function(event) {
     })
   );
 });
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => caches.delete(key))
+            );
+        })
+    );
+});
